c5c9b5ae83c1726a0542e13e6ddee6603eed538f
kameike <kameike@users.noreply.github.com>
2018-10-03 19:35:52 +0900

    upadte

--- misc/boot/mac.sh
+++ misc/boot/mac.sh
@@ -2,6 +2,10 @@
 
 #/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew install argon/mas/mas
+brew install rcmdnk/file/brew-file
+brew file install
+
 # brew install neovim/neovim/neovim
 # brew install python3
 # pip3 install neovim
