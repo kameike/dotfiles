e507a742466b2b3e31e6b42b862dc5bcbc9259de
kameike <kameike@users.noreply.github.com>
2019-04-09 22:47:32 +0900

    update bootfiles

--- bin/boot_mac.sh
+++ bin/boot_mac.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-set -eu
-
 if type brew > /dev/null 2>&1; then
     echo "brew has already installed"
 else
@@ -11,12 +9,11 @@ fi
 
 brew file install
 
-rm ~/.bash_profile
-rm ~/.bashrc
-rm ~/.gitconfig
-rm ~/.gitignore_global
-rm ~/.screenrc
-rm ~/.tmux.conf
+rm ~/.bash_profile > /dev/null  2>&1
+rm ~/.bashrc > /dev/null  2>&1
+rm ~/.gitconfig > /dev/null  2>&1
+rm ~/.gitignore_global > /dev/null  2>&1
+rm ~/.tmux.conf > /dev/null  2>&1
 
 ln -s ~/.config/rcs/bash_profile ~/.bash_profile
 ln -s ~/.config/rcs/bashrc ~/.bashrc
--- nvim/boot.sh
+++ nvim/boot.sh
@@ -1,7 +1,9 @@
 #!/bin/sh
 
-curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
-dir=`pwd`
+set -eu
+
+cd ~/.config/nvim/
 mkdir plugins
+curl https://raw.githubusercontent.com/Shougo/dein.vim/master/bin/installer.sh > installer.sh
 sh ./installer.sh plugins
 rm installer.sh
