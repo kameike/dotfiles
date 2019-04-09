049deb8c951f88a6f08b3c4ba5d31f666545d31f
kameike <kameike@users.noreply.github.com>
2019-04-09 23:02:35 +0900

    update bootmac

--- bin/boot_mac.sh
+++ bin/boot_mac.sh
@@ -11,17 +11,25 @@ if [ ! -d "$CONFIG" ]; then
 fi
 
 echo "start copy files"
-rm ~/.bash_profile > /dev/null  2>&1
-rm ~/.bashrc > /dev/null  2>&1
-rm ~/.gitconfig > /dev/null  2>&1
-rm ~/.gitignore_global > /dev/null  2>&1
-rm ~/.tmux.conf > /dev/null  2>&1
 
+rm ~/.bash_profile > /dev/null  2>&1
 ln -s ~/.config/rcs/bash_profile ~/.bash_profile
+
+rm ~/.bashrc > /dev/null  2>&1
 ln -s ~/.config/rcs/bashrc ~/.bashrc
+
+rm ~/.gitconfig > /dev/null  2>&1
 ln -s ~/.config/rcs/gitconfig ~/.gitconfig
+
+rm ~/.gitignore_global > /dev/null  2>&1
 ln -s ~/.config/rcs/gitignore_global ~/.gitignore_global
+
+rm ~/.tmux.conf > /dev/null  2>&1
 ln -s ~/.config/rcs/tmux.conf ~/.tmux.conf
+
+rm ~/.ssh/config> /dev/null  2>&1
+ln -s ~/.config/ssh/config ~/.ssh/config
+
 
 
 echo "install brew"
