76493275088dc8c3bbf8a19293967aba9070173b
kameike <kameike@users.noreply.github.com>
2019-04-14 20:12:31 +0900

    uopdate

--- bin/boot_mac.sh
+++ bin/boot_mac.sh
@@ -2,9 +2,16 @@
 
 CONFIG=$HOME/.config
 
-if [ ! $(xcode-select -p 2>/dev/null) ]; then
-  xcode-select --install
+
+echo "install brew"
+if type brew > /dev/null 2>&1; then
+    echo "brew has already installed"
+else
+    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 fi
+
+brew install argon/mas/mas
+brew install rcmdnk/file/brew-file
 
 if [ ! -d "$CONFIG" ]; then
   git clone https://github.com/kameike/env $CONFIG
@@ -31,17 +38,13 @@ rm ~/.ssh/config> /dev/null  2>&1
 ln -s ~/.config/ssh/config ~/.ssh/config
 
 
-
-echo "install brew"
-if type brew > /dev/null 2>&1; then
-    echo "brew has already installed"
-else
-    /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-fi
-
+sudo chown -R $(whoami) /usr/local/lib/pkgconfig
+chmod u+w /usr/local/lib/pkgconfig
 echo "install brew files"
 brew file install
 
 
 echo "setup nvim"
 sh ~/.config/nvim/boot.sh
+
+pip3 install neovim
