fd0d3f67936056d768696ee4512480c3f238f543
kameike <kameike@users.noreply.github.com>
2019-11-09 05:20:36 +0000

    add new files

---
+++ bin/linkrcs.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+set -eu
+
+rm ~/.bash_profile > /dev/null  2>&1
+ln -s ~/.config/rcs/bash_profile ~/.bash_profile
+
+rm ~/.bashrc > /dev/null  2>&1
+ln -s ~/.config/rcs/bashrc ~/.bashrc
+
+rm ~/.gitconfig > /dev/null  2>&1
+ln -s ~/.config/rcs/gitconfig ~/.gitconfig
+
+rm ~/.gitignore_global > /dev/null  2>&1
+ln -s ~/.config/rcs/gitignore_global ~/.gitignore_global
+
+rm ~/.tmux.conf > /dev/null  2>&1
+ln -s ~/.config/rcs/tmux.conf ~/.tmux.conf
+
+rm ~/.ssh/config> /dev/null  2>&1
+ln -s ~/.config/ssh/config ~/.ssh/config
+
