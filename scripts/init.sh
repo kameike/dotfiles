b2306f708a5e1a7eba861c8332de250151ac9302
kameike <kameike@users.noreply.github.com>
2020-05-19 14:57:56 +0900

    :bug:fix: Don't use super user when run script.

--- scripts/init.sh
+++ scripts/init.sh
@@ -2,11 +2,14 @@
 
 set -eu
 
+xcode-select --install || echo "ok"
+
 git -C ~/ clone https://github.com/kameike/dotfiles
 cd dotfiles
 
 make brew
 make install-basic-toolchain
+make install-biz-toolchain
 make sync-dotfiles
 
 
