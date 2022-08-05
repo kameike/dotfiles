6c245587003e5bae5c4e0d631fc67934a07692db
kameike <kameike@users.noreply.github.com>
2022-08-05 16:20:52 +0900

    update

--- scripts/sync_dotfiles.sh
+++ scripts/sync_dotfiles.sh
@@ -6,7 +6,7 @@ fi
 
 if !(type dotfiles > /dev/null 2>&1); then
 	echo "install dotfiles"
-	GO111MODULE=on go get github.com/rhysd/dotfiles
+	go install github.com/rhysd/dotfiles@latest
 fi
 
 gopath=$(go env | grep ^GOPATH | sed 's/GOPATH="\(.*\)"/\1/g')
